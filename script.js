0e92985d8759eade7543f601ae48609b99d54e8a
JureFilipovic <jure.filipovic@gmail.com>
2024-09-19 18:30:31 +0200

    Add logic for performing a calculations.
    Added event listeners for all the buttons and made
    the logic for the calculator to work as intended.

--- script.js
+++ script.js
@@ -14,17 +14,6 @@ function divide (a, b) {
     return a / b;
 }
 
-const log = console.log;
-
-// log (add (4.6, 2));
-// log (subtract (4.5, 2));
-// log (multiply (4.2, 2));
-// log (divide (5, 2));
-
-let firstNum;
-let operator;
-let secondNum;
-
 function operate (a, b, operator) {
     switch (operator) {
         case "+":
@@ -33,13 +22,114 @@ function operate (a, b, operator) {
         case "-":
             return subtract (a, b);
 
-        case "*":
+        case "x":
             return multiply (a, b);
 
-        case "/":
+        case "÷":
+            if (b === 0)  {
+                return "Can't do division with 0!"
+            }
             return divide (a, b);
 
         default:
             return "Invalid operator";
     }
 }
+
+function formatResult (result) {
+    if (Number.isInteger (result)) {
+        return result.toString ();
+
+    } else {
+        return parseFloat (result.toFixed(10).toString());
+    }
+}
+
+const digitButtons = document.querySelectorAll(".digits .digitButton");
+const operationButtons = document.querySelectorAll (".operations .operationButton");
+const controlButtons = document.querySelectorAll (".controls .controlButton");
+const inputOutputField = document.querySelector (".inputOutputField");
+
+let firstNumber = "";
+let secondNumber = "";
+let operation = "";
+let isOperation = false;
+let result = null;
+
+digitButtons.forEach (button => {
+    button.addEventListener ("click", () => {
+        if (isOperation) {
+            inputOutputField.value = "";
+            isOperation = false;
+        }
+        
+        if (operation === "") {
+            firstNumber += button.textContent;
+            inputOutputField.value += button.textContent;
+
+        } else {
+            secondNumber += button.textContent;
+            inputOutputField.value += button.textContent;
+        }
+    });
+});
+
+operationButtons.forEach (button => {
+    button.addEventListener ("click", () => {
+        if (firstNumber != "" && secondNumber != "") {
+            let tempResult = 
+                operate (Number (firstNumber), Number (secondNumber), operation);
+            
+            if (tempResult === "Can't do division with 0!") {
+                result = tempResult;
+                inputOutputField.value = result;
+                firstNumber = "";
+                secondNumber = "";
+
+            } else {
+                result = formatResult (tempResult);
+                inputOutputField.value = result;
+                firstNumber = String (result);
+                secondNumber = "";
+            }
+        }  
+
+        operation = button.textContent;
+        isOperation = true;
+    });
+});
+
+controlButtons.forEach (button => {
+    button.addEventListener ("click", () => {
+        if (button.textContent === "=") {
+            if (firstNumber != "" && secondNumber != "" && operation != "") {
+                let tempResult = 
+                    operate (Number (firstNumber), Number (secondNumber), operation);
+                
+                if (tempResult === "Can't do division with 0!") {
+                    result = tempResult;
+                    inputOutputField.value = result;
+                    firstNumber = "";
+                    secondNumber = "";
+                    operation = "";
+                    isOperation = false;
+
+                } else {
+                    result = formatResult (tempResult);
+                    inputOutputField.value = result;
+                    firstNumber = String (result);
+                    secondNumber = "";
+                    operation = "";
+                    isOperation = false;
+                }
+            }
+
+        } else if (button.textContent === "CE") {
+            inputOutputField.value = "";
+            firstNumber = "";
+            secondNumber = "";
+            operation = "";
+            isOperation = false;
+        }
+    });
+});
